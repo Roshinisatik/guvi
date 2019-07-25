85be45f02e0db8eca9d8212cb02df6423ec35266
Roshinisatik <51362071+Roshinisatik@users.noreply.github.com>
2019-07-25 12:48:14 +0530

    Create proex16.py

---
+++ codes/proex16.py
@@ -0,0 +1,11 @@
+a6=int(input())
+c6=list(map(int,input().split()))
+x6=[1]*a6
+for q in range(a7):
+    if q==0:
+        if c6[q]>c6[q+1]:
+            x6[p]=x6[q]+x6[q+1]
+    elif q>0:
+        if c6[q]>c6[q-1]:
+            x6[q]=x6[q]+x6[q-1]
+print(sum(x6))
