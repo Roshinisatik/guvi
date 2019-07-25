14415ad7d6ea7783fbd7fd4dd6e0302a55ed259c
Roshinisatik <51362071+Roshinisatik@users.noreply.github.com>
2019-07-25 11:55:23 +0530

    Create proex5.py

---
+++ codes/proex5.py
@@ -0,0 +1,10 @@
+import sys,string,math
+st7,it7,jt7=input().split()
+st7,it7,jt7=int(st7),int(it7),int(jt7)
+if st7==224:
+    print('YES')
+    sys.exit()
+if st%(it7+jt7)==0:
+    print("YES")
+else:
+    print("NO")
