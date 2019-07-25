8ebb2373556c483329b4ebb69a9929dccba7c30a
Roshinisatik <51362071+Roshinisatik@users.noreply.github.com>
2019-07-25 12:28:43 +0530

    Create proex12.py

---
+++ codes/proex12.py
@@ -0,0 +1,11 @@
+a6,b6=map(int,input().split())
+lisseq=list(map(int,input().split()))
+l6=[]
+for i in range(0,b6):
+     u6,v6=map(int,input().split())
+     l7.append([u6,v6])
+for i in range(b6):
+     lower1=l6[i][0]
+     upper1=l6[i][1]
+     s6=sum(lisseq[lower-1:upper])
+     print(s6)
